d3947ba4c0fdbdcecae7c3d5661c3474b762eeb0
Litzy Xu <litzyxuye@gmail.com>
2021-04-05 01:49:23 +0000

    added StarWars ERD (Entity Relationship Diagram)

--- src/models.py
+++ src/models.py
@@ -8,26 +8,57 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = "user"
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    uid = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    lastname = Column(String(50))
+    username = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    fav_characters = relationship("Fav_Character", back_populates="user") # One to Many. One user can have many fav_character
+    fav_planets = relationship("Fav_Planet", back_populates="user") # One to Many. One user can have many fav_planet
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    # Defining a method inside the class is OPTIONAL
+    # The %s operator lets you add a value into a Python string
+    def serialize(self):
+        return "<User(name='%s', lastname='%s', username='%s')>" % (self.name, self.lastname, self.username)
 
-    def to_dict(self):
-        return {}
+
+class Character(Base):
+    __tablename__ = "character"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    gender = Column(String(50), nullable=False)
+    eye_color = Column(String(50), nullable=False)
+    hair_color = Column(String(50), nullable=False)
+    fav_character = relationship("Fav_Character", uselist=False, back_populates="character") # One to One. One character is associated with One fav_character.id
+
+class Fav_Character(Base):
+    __tablename__ = "fav_character"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id")) # One to Many. One user can have many favorites
+    character_id = Column(Integer, ForeignKey("character.id")) # One to One. One character is associated with One fav_character.id
+    character = relationship("Character", back_populates="fav_character") # One to One. One character is associated with One fav_character.id
+
+
+class Planet(Base):
+    __tablename__ = "planet"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    population = Column(Integer, nullable=False)
+    terrain = Column(String(50), nullable=False)
+    climate = Column(String(50), nullable=False)
+    fav_planet = relationship("Fav_Planet", uselist=False, back_populates="planet") # One to One. One planet is associated with One fav_planet.id
+
+class Fav_Planet(Base):
+    __tablename__ = "fav_planet"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id")) # One to Many. One user can have many favorites
+    planet_id = Column(Integer, ForeignKey("planet.id")) # One to One. One planet is associated with One fav_planet.id
+    planet = relationship("Planet", back_populates="fav_planet") # One to One. One planet is associated with One fav_planet.id
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
